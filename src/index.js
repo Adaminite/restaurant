df3f2d87ad19f0b772390e8c86d0eb333590e13c
Adaminite <niazi.omark@gmail.com>
2022-01-14 16:50:40 -0500

    Add necessary files and display controller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("Hi. Webpack works");
+import renderHome from "./home.js";
+
+const displayController = ( () => {
+
+    let content = document.getElementById("content");
+
+    const renderLanding = () => {
+        content.appendChild(createButtonDiv());
+        content.appendChild(renderHome());
+    };
+
+    const createButtonDiv = () => {
+
+        let buttonDiv = document.createElement('div');
+
+        const buttonNames = ["Home", "Menu", "Contact"];
+
+        for(let buttonName of buttonNames){
+            let btn = document.createElement('button');
+            btn.textContent = buttonName;
+            btn.addEventListener('click', renderPage);
+            buttonDiv.appendChild(btn);
+        }
+        
+        return buttonDiv;
+
+    }
+
+    const renderPage = (e) => {
+        let pageName = e.target.textContent;
+        console.log(pageName);
+    };
+})();
+
+displayController.renderLanding();
