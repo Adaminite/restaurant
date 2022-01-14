95798896cd2f8efcd609d031c0cbb15951efc633
Adaminite <niazi.omark@gmail.com>
2022-01-14 18:41:02 -0500

    Add page mapper to display controller

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import home from "./home.js";
+import contact from "./contact.js";
+import menu from "./menu.js";
 
 const displayController = ( () => {
 
     let content = document.getElementById("content");
+    let currPage = "";
+
+    const pageMapper = {
+        "Home": home(),
+        "Contact": contact(),
+        "Menu": menu()
+    };
 
     const renderLanding = () => {
         content.appendChild(createButtonDiv());
         content.appendChild(home());
+
+        currPage = "Home";
     };
 
     const createButtonDiv = () => {
@@ -24,11 +35,19 @@ const displayController = ( () => {
         
         return buttonDiv;
 
-    }
+    };
 
     const renderPage = (e) => {
         let pageName = e.target.textContent;
-        console.log(pageName);
+
+        if(pageName !== currPage){
+            content.removeChild(content.lastElementChild);
+
+            content.appendChild( pageMapper[pageName]);
+
+            currPage = pageName;
+        }
+
     };
 
     return {renderLanding};
